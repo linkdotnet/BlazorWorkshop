a5b1d6e882481344051d6681edb58e71e791ba9a
Steven Giesel <stgiesel35@gmail.com>
2020-10-09 13:59:38 +0200

    Initial task

---
+++ LinkDotNet.BlazorWorkshop/Client/Shared/Counter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LinkDotNet.BlazorWorkshop.Client.Shared
+{
+    public partial class Counter
+    {
+        [Parameter]
+        public int CurrentCount { get; set; }
+
+        private void IncrementCount()
+        {
+            CurrentCount++;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
