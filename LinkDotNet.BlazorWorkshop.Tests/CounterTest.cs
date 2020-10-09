404e7979e233806a2b5fee7823cb654a0a68a5d7
Steven Giesel <stgiesel35@gmail.com>
2020-10-09 11:58:03 +0200

    Added infrastructure for task 2

---
+++ LinkDotNet.BlazorWorkshop.Tests/CounterTest.cs
@@ -0,0 +1,44 @@
+﻿using Bunit;
+using Bunit.Rendering;
+using LinkDotNet.BlazorWorkshop.Client.Pages;
+using LinkDotNet.BlazorWorkshop.Client.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkDotNet.BlazorWorksop.Tests
+{
+    [TestClass]
+    public class CounterTest : ComponentTestBase
+    {
+        [TestMethod]
+        public void GivenCounter_WhenSettingState_ThenCounterShouldStartThere()
+        {
+            // Arrange
+            var componentParameter = ComponentParameter.CreateParameter("CurrentCount", 100);
+            var cut = RenderComponent<Counter>(componentParameter);
+            cut.SaveSnapshot();
+
+            // Act
+            cut.Find("button").Click();
+
+            // Assert
+            var currentCount = cut.GetChangesSinceSnapshot().ShouldHaveSingleChange();
+            currentCount.ShouldBeTextChange("Current count: 101");
+        }
+
+        [TestMethod]
+        public void GivenCounter_WhenAddingNegativeValues_ThenSetBackToZero()
+        {
+            // Arrange
+            var componentParameter = ComponentParameter.CreateParameter("CurrentCount", -100);
+            var cut = RenderComponent<Counter>(componentParameter);
+            cut.SaveSnapshot();
+
+            // Act
+            cut.Find("button").Click();
+
+            // Assert
+            var currentCount = cut.GetChangesSinceSnapshot().ShouldHaveSingleChange();
+            currentCount.ShouldBeTextChange("Current count: 1");
+        }
+    }
+}
